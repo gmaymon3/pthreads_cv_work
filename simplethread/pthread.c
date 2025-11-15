c059f01dc88ab5b76ad577a7025858b46fedc142
Gabe Maymon <gabemaymon@gmail.com>
2025-11-14 22:01:41 -0500

    Edited simple thread to have sys log
    Sys log prevents the need for fprint that may slow the task

--- simplethread/pthread.c
+++ simplethread/pthread.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sched.h>
+#include <syslog.h>
+#include <sys/utsname.h>  
 
 #define NUM_THREADS 12
 
@@ -19,15 +21,19 @@ threadParams_t threadParams[NUM_THREADS];
 
 void *counterThread(void *threadp)
 {
+    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT:1] Hello World from Thread!");
+
     int sum=0, i;
     threadParams_t *threadParams = (threadParams_t *)threadp;
 
     for(i=1; i < (threadParams->threadIdx)+1; i++)
         sum=sum+i;
  
-    printf("Thread idx=%d, sum[0...%d]=%d\n", 
+    // Write to syslog instead of printf
+    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT:1] Thread idx=%d, sum[0...%d]=%d",
            threadParams->threadIdx,
-           threadParams->threadIdx, sum);
+           threadParams->threadIdx,
+           sum);
 }
 
 
@@ -35,6 +41,24 @@ int main (int argc, char *argv[])
 {
    int rc;
    int i;
+
+    // Open syslog
+    openlog("pthread", LOG_PID | LOG_CONS, LOG_USER);
+    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT:1] Hello World from Main!");
+
+    // Print uname -a first
+    struct utsname sysinfo;
+    if (uname(&sysinfo) == 0) {
+        char unameStr[512];
+        snprintf(unameStr, sizeof(unameStr), "%s %s %s %s %s",
+                 sysinfo.sysname,
+                 sysinfo.nodename,
+                 sysinfo.release,
+                 sysinfo.version,
+                 sysinfo.machine);
+
+        syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT:1] %s", unameStr);
+    }
 
    for(i=0; i < NUM_THREADS; i++)
    {
@@ -51,5 +75,8 @@ int main (int argc, char *argv[])
    for(i=0;i<NUM_THREADS;i++)
        pthread_join(threads[i], NULL);
 
-   printf("TEST COMPLETE\n");
+    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT:1] TEST COMPLETE");
+
+    closelog(); // Close syslog
+
 }
